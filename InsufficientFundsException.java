11334931c70a3a53f3b726e8dcca3a7daf232f8d
Mohamed Idris <139785872+mohaidris@users.noreply.github.com>
2026-06-09 21:14:13 +0300

    Delete InsufficientFundsException.java

--- InsufficientFundsException.java
+++
@@ -1,6 +0,0 @@
-// Question 3: Custom Exception extending Exception class
-public class InsufficientFundsException extends Exception {
-    public InsufficientFundsException(String message) {
-        super(message);
-    }
-}
