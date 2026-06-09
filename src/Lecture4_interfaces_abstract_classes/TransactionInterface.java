5ef77e28337be5e47e7d5ac60f2e1bb27e25b8f2
Mohamed Idris <139785872+mohaidris@users.noreply.github.com>
2026-06-09 21:05:04 +0300

    Update TransactionInterface.java

--- src/Lecture4_interfaces_abstract_classes/TransactionInterface.java
+++ src/Lecture4_interfaces_abstract_classes/TransactionInterface.java
@@ -1,21 +1,9 @@
-package Lecture4_interfaces_abstract_classes;
 import java.util.Calendar;
 
-/**
- * Interface for Transactions
- * Any class that defines a transaction is expected to implement this Interface
- */
 public interface TransactionInterface {
-
-    // Method to get the transaction amount
     double getAmount();
-
-    // Method to get the transaction date
     Calendar getDate();
-
-    // Method to get a unique identifier for the transaction
     String getTransactionID();
-
+    void printTransactionDetails();
+    void apply(BankAccount ba) throws InsufficientFundsException;
 }
-
-
